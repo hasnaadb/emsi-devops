92899a94ed5bd0670932882359aa22629cb722a4
hasnaadb <80129167+hasnaadb@users.noreply.github.com>
2021-05-19 07:13:15 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x03-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x03-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,25 @@
-#!/usr/bin/env bash
+# Creating a custom Header HTTP with Puppet
+
+exec { 'update':
+  command => 'sudo apt-get update',
+  path    => ['/usr/bin', '/bin'],
+}
+
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['update'],
 }
 
-exec { 'http header':
-  command  => '
-	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-	sudo service nginx restart',
-  provider => shell,
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-content => 'Holberton School',
-}
-
-service { 'nginx':
-  ensure  => running,
+file_line { 'custom_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
   require => Package['nginx'],
+}
+
+exec { 'restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  require => File_line['custom_header'],
 }
